906cbf77559e19c88f704bf25df67fd4c4e2ac63
freelancing-solutions <mobiusndou@gmail.com>
2023-06-15 12:39:41 +0200

    added building mainetenance

--- src/routes/buildings.py
+++ src/routes/buildings.py
@@ -15,4 +15,9 @@ async def get_building(building_id: str):
     :param building_id:
     :return:
     """
-    return render_template('building/buildings.html')
+    return render_template('building/building.html')
+
+
+@buildings_route.get('/admin/add-building')
+async def add_building():
+    return render_template('building/add_building.html')
---
+++ src/routes/reports.py
@@ -0,0 +1,9 @@
+from flask import Blueprint, render_template
+
+reports_route = Blueprint('reports', __name__)
+
+
+@reports_route.get('/admin/reports')
+async def get_maintenance():
+    return render_template('reports/reports.html')
+
