84c41fb92e19aa1fb512858e3694e804ba254088
freelancing-solutions <mobiusndou@gmail.com>
2023-06-16 20:27:45 +0200

    updated company forms

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-import uvicorn
 from src.config import config_instance
 from src.main import create_app
 
 app = create_app(config=config_instance())
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000, debug=True, extra_files=['src', 'templates', 'static'])
 
-    app.run(host='0.0.0.0', port=8000)
