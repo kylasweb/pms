8cd5c1be92eeaab3cd462bc525498ade508bafdc
freelancing-solutions <mobiusndou@gmail.com>
2023-06-17 20:01:18 +0200

    created a user registration module that will auto login the user after creating a registration

--- src/database/models/users.py
+++ src/database/models/users.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel
+import uuid
+from pydantic import BaseModel, Field
 from enum import Enum
 
 
 class UserType(str, Enum):
-    ADMIN = "admin"
-    CUSTOMER = "customer"
-    STAFF = "staff"
+    ADMIN = 'admin'
+    TENANT = 'tenant'
 
 
 class User(BaseModel):
@@ -15,20 +15,22 @@ class User(BaseModel):
     Attributes:
     - user_id (str): The ID of the user.
     - company_id (str): The ID of the company associated with the user.
+    - is_tenant (bool): Indicates if the user is a tenant.
+    - tenant_id (str): The ID of the tenant associated with the user.
     - user_type (UserType): The type of user.
     - username (str): The username of the user.
+    - password (str | None): The password of the user.
     - email (str): The email address of the user.
     - full_name (str): The full name of the user.
     - contact_number (str): The contact number of the user.
     """
-    user_id: str
+    user_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     company_id: str
     is_tenant: bool
     tenant_id: str
-
     user_type: UserType
     username: str
-    password: str | None
+    password: str = None
     email: str
     full_name: str
     contact_number: str
--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,5 +1,7 @@
 from datetime import datetime, timedelta
 from flask import Blueprint, render_template, request, make_response, flash, redirect, url_for
+
+from src.database.models.users import User
 from src.view.auth import UserView
 
 auth_route = Blueprint('auth', __name__)
@@ -61,6 +63,29 @@ async def do_logout():
 @auth_route.get('/admin/register')
 async def get_register():
     return render_template('register.html')
+
+
+@auth_route.post('/admin/register')
+async def do_register():
+    username = request.form.get('username')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # user_id will already be created by a factory
+    user_data = User()
+    user_data.username = username
+    user_data.password = password
+    user_data.email = email
+    user_view = UserView()
+    user_data: User = await user_view.post(user=user_data)
+
+    response = make_response(render_template('companies/companies.html'))
+    # Calculate expiration time (e.g., 30 minutes from now)
+    expiration = datetime.utcnow() + timedelta(minutes=30)
+    # Set the authentication cookie
+    response.set_cookie('auth', value=user_data.user_id, expires=expiration, httponly=True)
+    # Return the response
+    return response
 
 
 @auth_route.get('/admin/password-reset')
