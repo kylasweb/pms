b6a2df9ee788872b3e1f482a2b59ab0e77133ab8
freelancing-solutions <mobiusndou@gmail.com>
2023-06-17 15:58:38 +0200

    refactored BaseModels adding functionality to ensure id generations

--- src/database/models/tenants.py
+++ src/database/models/tenants.py
@@ -1,9 +1,10 @@
+import uuid
 from datetime import date
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Tenant(BaseModel):
-    tenant_id: str
+    tenant_id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="The unique ID of tenant.")
     name: str
     email: str
     phone_number: str
