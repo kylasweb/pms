facc2c09cf27f1860a4926d81587a6de54692773
freelancing-solutions <mobiusndou@gmail.com>
2023-06-17 16:01:58 +0200

    added lease Agreement Models

--- src/database/models/properties.py
+++ src/database/models/properties.py
@@ -30,3 +30,28 @@ class Unit(BaseModel):
     lease_end_date: date
     unit_area: int
     has_reception: bool
+
+
+class LeaseAgreement(BaseModel):
+    """
+    Represents a lease agreement.
+
+    Attributes:
+    - agreement_id (str): The ID of the lease agreement.
+    - property_id (str): The ID of the property associated with the agreement.
+    - tenant_id (str): The ID of the tenant associated with the agreement.
+    - start_date (date): The start date of the lease.
+    - end_date (date): The end date of the lease.
+    - rent_amount (float): The monthly rent amount.
+    - deposit_amount (float): The security deposit amount.
+    - is_active (bool): Indicates if the lease agreement is currently active.
+    """
+
+    agreement_id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="Agreement ID")
+    property_id: str
+    tenant_id: str
+    start_date: date
+    end_date: date
+    rent_amount: float
+    deposit_amount: float
+    is_active: bool
--- src/database/models/tenants.py
+++ src/database/models/tenants.py
@@ -4,7 +4,21 @@ from pydantic import BaseModel, Field
 
 
 class Tenant(BaseModel):
-    tenant_id: str = Field(default_factory=lambda: str(uuid.uuid4()), description="The unique ID of tenant.")
+    """
+    Represents a tenant.
+
+    Attributes:
+    - tenant_id (str): The unique ID of the tenant.
+    - name (str): The name of the tenant.
+    - email (str): The email address of the tenant.
+    - phone_number (str): The phone number of the tenant.
+    - address_id (str): The ID of the address associated with the tenant.
+    - lease_start_date (date): The start date of the lease.
+    - lease_end_date (date): The end date of the lease.
+    """
+
+    tenant_id: str = Field(default_factory=lambda: str(uuid.uuid4()),
+                           description="The unique ID of the tenant.")
     name: str
     email: str
     phone_number: str
