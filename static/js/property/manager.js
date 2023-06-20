38c954ea3ad3f4eb153790d6d11e47308d90c566
freelancing-solutions <mobiusndou@gmail.com>
2023-06-20 13:19:06 +0200

    added a property editor

--- static/js/property/manager.js
+++ static/js/property/manager.js
@@ -1,14 +1,22 @@
+
+window.addEventListener('load', () => {
     const container = document.getElementById("add-unit-container");
     const button = document.getElementById("add-unit-btn");
 
     button.addEventListener("click", function() {
-        if (container.style.display === "none") {
-            container.style.display = "block";
+        if (container.classList.contains('hidden')) {
+            container.classList.remove("hidden");
+            container.classList.add("visible");
             button.innerHTML = "<i class='ti-close'> </i> Close Editor";
             button.className = "btn btn-danger"
         } else {
-            container.style.display = "none";
+            container.classList.add("hidden");
+            container.classList.remove("visible");
             button.innerHTML = "<i class='ti-folder'></i> Open Unit Editor";
             button.className = "btn btn-primary";
         }
     });
+})
+
+
+
