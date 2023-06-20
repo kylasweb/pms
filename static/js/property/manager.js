4ce040c6cdf408051cbe3c810bbd2ba7e80fce6a
freelancing-solutions <mobiusndou@gmail.com>
2023-06-20 13:45:09 +0200

    refactored - redisigning the interface

--- static/js/property/manager.js
+++ static/js/property/manager.js
@@ -8,12 +8,12 @@ window.addEventListener('load', () => {
             container.classList.remove("hidden");
             container.classList.add("visible");
             button.innerHTML = "<i class='ti-close'> </i> Close Editor";
-            button.className = "btn btn-danger"
+            button.className = "btn btn-sm btn-danger mr-2"
         } else {
             container.classList.add("hidden");
             container.classList.remove("visible");
             button.innerHTML = "<i class='ti-folder'></i> Open Unit Editor";
-            button.className = "btn btn-primary";
+            button.className = "btn btn-sm btn-primary mr-2";
         }
     });
 })
