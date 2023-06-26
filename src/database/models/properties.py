166ce5b48fe36d5b11e073d08bff75a213eaed52
freelancing-solutions <mobiusndou@gmail.com>
2023-06-26 15:14:10 +0200

    finishing lease agreements creation

--- src/database/models/properties.py
+++ src/database/models/properties.py
@@ -94,7 +94,7 @@ class Unit(BaseModel):
 
 class AddUnit(BaseModel):
     property_id: str
-    unit_id: str
+    unit_id: str = Field(default_factory=lambda: str(uuid.uuid4()))
     unit_number: str
     rental_amount: int
     unit_area: int
